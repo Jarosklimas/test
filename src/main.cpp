513a230ee9f029ece17ab9ad92373a13f57ad0a2
Andrzej Kaczorowski <andrzej.kaczorowski@mobica.com>
2018-04-09 12:30:48 +0200

    strukturka klasy kalkulator
    Signed-off-by: Andrzej Kaczorowski <andrzej.kaczorowski@mobica.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,57 @@
-int main()
+#include<iostream>
+
+#include "main.hpp"
+
+Kalkulator::Kalkulator(const int& a, const int& b) : 
+   m_a(a),
+   m_b(b)
 {
-   cout << "sss";
+   std::cout << "Nowy obiekt kalkulatora stworzony!" << std::endl;
+   std::cout << "argument 1: " << m_a << std::endl;
+   std::cout << "argument 2: " << m_b << std::endl;
+}
+
+int Kalkulator::wykonajDzialanie(const Tryb& tryb)
+{
+   int ret;
+   std::string dzialanie;
+
+   if(Tryb::Dodawanie == tryb)
+   {
+      dzialanie = "dodawanie";
+      ret = m_a + m_b;
+   }
+   else if(Tryb::Odejmowanie == tryb)
+   {
+      //TODO: dodać implementację dla pozsotałych działań
+   }
+
+   std::cout << "Wybrane dzialanie: " << dzialanie << std::endl;
+
+   return ret;
+}
+
+int main(int argc, char** argv)
+{
+   if(argc < 3)
+   {
+      std::cout << "Zbyt mała liczba argumentów!" << std::endl;
+   }
+   else
+   {
+      int arg1 = atoi(argv[1]);
+      int arg2 = atoi(argv[2]);
+
+      Kalkulator calc(arg1, arg2);
+      int wynik = calc.wykonajDzialanie(Tryb::Dodawanie);
+
+      std::cout << "Wynik: " << wynik << std::endl;
+
+
+;
+      
+
+      //std::cout << "Wynik: " << result << std::endl;
+   }
+
 }
--- src/main.hpp
+++ src/main.hpp
@@ -1,4 +1,16 @@
-void main ();
-void moj_komentarz(int k, int b);
-void pierwsza_funkcja(int b, int g);
+enum class Tryb {
+   Dodawanie = 1,
+   Odejmowanie = 2,
+   Mnozenie = 3,
+   Dzielenie = 4
+};
 
+class Kalkulator{
+public:   
+   Kalkulator(const int& a, const int& b);
+   int wykonajDzialanie(const Tryb& tryb); 
+
+private:
+   int m_a;
+   int m_b;
+};
