3e5caef26208e70401160d50a93882d1e139ae30
Jaroslaw Klimczyk <Jaroslaw.Klimczyk@harman.com>
2020-09-22 17:33:37 +0200

    dodanie lambda

--- Python/lambda.py
+++ Python/lambda.py
@@ -2,3 +2,8 @@ suma = lambda a, b : a + b
 
 wynik =suma (3, 5)
 print(wynik)
+def wypisz_informacje(**kwargs): #kwargs to jest inaczej słownik
+    for klucz in kwargs:
+        print(f'Pod kluczem {klucz} znajduje sie {kwargs[klucz]}.')
+
+wypisz_informacje (imie='Jarek', nazwisko='Klimczyk')
