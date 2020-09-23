f000487943d4298a5ade95625d15ae5b0ad3cf22
Jaroslaw Klimczyk <Jaroslaw.Klimczyk@harman.com>
2020-09-23 15:51:27 +0200

    Nowy projekt z kalkulatorem (problem w funkcja 'connect' dla button

--- Python/zadanie_1.py
+++ Python/zadanie_1.py
@@ -1,17 +1,36 @@
 import calendar as c
+import random as r
+
+
 
 name = input ('Podaj imię  \n') 
-
-print ('dwie pierwsze litery', name[:2])
+print ('Dwie pierwsze litery', name[:2])
 print (name[0]+ name[1])
-
 if name[-1]== 'a':
     print('Imie jest żeński')
 else:
         print('Imie jest meskie')
 
-pesel = input('Podaj pesel\n')
-#print ('Ostatnia cyfra liczy ', pesel, 'to' , pesel%10)
-print ('Rocznik ', pesel[0:2], 'urodzony', pesel[2:4], "w dniu", pesel[4:6])
-
-print(c.month (2016,2))
+pesel = int(input('Podaj pesel\n'))
+print ('Ostatnia cyfra liczby ', pesel, 'to', pesel%10)
+if (pesel%10) == 1 :
+    print("Jestes osobnikiem męskim")
+else:
+    print('Jestes osobnikiem żenśkim')
+numer = str(pesel)
+print ('Rocznik \n', numer[0:2], '\n', 'urodzony \n', numer[2:4], " \n", "w dniu \n", numer[4:6])
+y = int (input("Podaj rok :"))
+m = int (input("Podaj miesiac :"))
+print (c.month(y,m))
+                                        #prosi o podanie hasła 
+word = input ("Podaj hasło \n ")
+if word.lower() =='akademia':
+    print("Hasło poprawne")
+else:
+    print("Hasło niepoprawne")
+                                        # dopóki nie podasz poprawnego hasła program bedzie wykonywał petle while. Jesli uzytkownik nie odgadnie hasła bedzie komunikat podaj jeszcze raz hasło
+password =input('Podaj hasło jeszcze raz dla uwierzytelnienia użytkownika \n')
+secret_password = 'Akademia'
+while password!= secret_password:
+    password = input ('Niepoprawne hasło podaj jeszcze raz \n')
+print('Gratulacje podałes poprawne hasło ')
